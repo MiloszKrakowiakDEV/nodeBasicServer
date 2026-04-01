97128c4d676793ac1fb709db1c4a849c753afba3
MiloszKrakowiakDEV <136705198+MiloszKrakowiakDEV@users.noreply.github.com>
2026-04-01 21:59:57 +0200

    Added password hashing using bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const http = require('http');
 const mysql = require('mysql2/promise');
+const http = require("http");
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10;
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -15,9 +19,11 @@ const pool = mysql.createPool({
 
 
 const server = http.createServer(async (req, res) => {
-  console.log("Got a request")
+  console.log("Got a request");
+
   if (req.method == "POST") {
     switch (req.url) {
+
       case "/user/add":
         let body = "";
 
@@ -27,31 +33,31 @@ const server = http.createServer(async (req, res) => {
 
         req.on("end", async () => {
           try {
-            console.log(body)
+            console.log(body);
             const data = JSON.parse(body);
+
+            const hashedPassword = await bcrypt.hash(data.password, SALT_ROUNDS);
+
             const connection = await pool.getConnection();
-           await connection.execute(
-              `INSERT INTO users(username, password,points) VALUES (?, ?, ?)`,
-              [data.username, data.password,data.points]
+
+            await connection.execute(
+              `INSERT INTO users(username, password, points) VALUES (?, ?, ?)`,
+              [data.username, hashedPassword, data.points]
             );
+
             connection.release();
-            res.end("Added user: " + data.username + " " + data.password + " "+ data.points);
-          }
-          catch (err) {
+
+            res.end("Added user: " + data.username);
+
+          } catch (err) {
             console.error(err);
             res.writeHead(500, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ error: 'Internal server error' }));
           }
-
         });
 
+        break;
     }
-  } else {
-    switch (req.url) {
-
-      
-    }
-
   }
 });
 
